700e339e9b94f68910bb98615c727da4bd2aa69b
Sean Earle <sean.r.earle@gmail.com>
2022-04-02 11:51:15 +1030

    Use the actual class' variables, not the module for the state machine

--- core/state_machine.rb
+++ core/state_machine.rb
@@ -2,10 +2,10 @@ module StateMachine
   def self.included(klass)
     klass.class_eval do
       def self.state_machine(states: {}, methods: [])
-        @@state_map = states
-        @@state_methods = methods
+        self.class_variable_set(:@@state_map, states)
+        self.class_variable_set(:@@state_methods, methods)
 
-        @@state_map.each { |state_key, state_klass|
+        self.class_variable_get(:@@state_map).each { |state_key, state_klass|
           define_method "#{state_key}?" do
             state.class == state_klass
           end
@@ -16,15 +16,23 @@ module StateMachine
 
   attr_accessor :state
 
+  def state_map
+    self.class.class_variable_get(:@@state_map)
+  end
+
+  def state_methods
+    self.class.class_variable_get(:@@state_methods)
+  end
+
   def transition_to(next_state, *args)
-    unless @@state_map.keys.include?(next_state)
-      raise ArgumentError.new("Unknown state #{next_state}, valid states are: #{@@state_map.keys}")
+    unless state_map.keys.include?(next_state)
+      raise ArgumentError.new("Unknown state #{next_state}, valid states are: #{state_map.keys}")
     end
-    @state = @@state_map[next_state].new(*args)
+    @state = state_map[next_state].new(*args)
   end
 
   def method_missing(method, *args, &block)
-    if @@state_methods.include?(method)
+    if state_methods.include?(method)
       state.send(method, *args, &block)
     else
       raise NoMethodError.new("Undefined method #{method} for #{self} and not white listed to pass to the state")
